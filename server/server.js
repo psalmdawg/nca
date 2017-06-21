5bdb4303fe9292e3624f904b094d0bb8792e607b
Paul H Salmon <Pauly@MacBook-Pro.local>
2017-06-22 09:42:20 +1000

    first commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express();
+const path = require('path');
+
+const publicPath = path.join(__dirname, '../public')
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath))
+
+
+app.listen(port, function(){
+  console.log('live on ', port)
+})
